534a719051995ce227289a43dae41cc3a832ce91
Otthon Leão <54039360+otthonleao@users.noreply.github.com>
2023-10-27 16:11:44 -0400

    feat: add iQuiz app corinthians teste gpg

--- iQuiz/Modelo/Questao.swift
+++ iQuiz/Modelo/Questao.swift
@@ -14,7 +14,7 @@ struct Questao {
 }
 
 let questoes: [Questao] = [
-    Questao(titulo: "Quantos Brasileirão o Corinthians tem?", respostas: ["5", "8", "7"], respostaCorreta: 2),
+    Questao(titulo: "Quantos Brasileirão o Corinthians tem???????", respostas: ["5", "8", "7"], respostaCorreta: 2),
     Questao(titulo: "Quantos títulos da Copa do Brasil o Corinthians tem?", respostas: ["2", "3", "1"], respostaCorreta: 1),
     Questao(titulo: "Qual foram os anos que o Corinthians ganhou o Mundial?", respostas: ["2020 e 2021", "2002 e 2012", "2000 e 2012"], respostaCorreta: 2),
 
